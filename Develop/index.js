09a99c65ef0882bfe50716450549e6e360ac0cd3
Apgash <ashtonvilkosi@gmail.com>
2024-10-05 12:41:27 -0400

    adds generate markdown code for readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,71 +1,87 @@
 // TODO: Include packages needed for this application
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
 // TODO: Create an array of questions for user input
-const questions = ['Title of your project', 'Description of your project', 'Installation instructions', 'Usage information', 'Contribution guidelines', 'Test instructions', 'GitHub username', 'Email address' , 'Table of contents', 'License'];
+const questions = [
+  "Title of your project",
+  "Description of your project",
+  "Installation instructions",
+  "Usage information",
+  "Contribution guidelines",
+  "Test instructions",
+  "GitHub username",
+  "Email address",
+  "Table of contents",
+  "License",
+];
 
 inquirer
   .prompt([
     {
-        type: 'input',
-        name: 'title',
-        message: questions[0],
+      type: "input",
+      name: "title",
+      message: questions[0],
     },
     {
-        type: 'input',
-        name: 'description',
-        message: questions[1],
+      type: "input",
+      name: "description",
+      message: questions[1],
     },
     {
-        type: 'input',
-        name: 'installation',
-        message: questions[2],
+      type: "input",
+      name: "installation",
+      message: questions[2],
     },
     {
-        type: 'input',
-        name: 'usage',
-        message: questions[3],
+      type: "input",
+      name: "usage",
+      message: questions[3],
     },
     {
-        type: 'input',
-        name: 'contribution',
-        message: questions[4],
+      type: "input",
+      name: "contribution",
+      message: questions[4],
     },
     {
-        type: 'input',
-        name: 'test',
-        message: questions[5],
+      type: "input",
+      name: "test",
+      message: questions[5],
     },
     {
-        type: 'input',
-        name: 'github',
-        message: questions[6],
+      type: "input",
+      name: "github",
+      message: questions[6],
     },
     {
-        type: 'input',
-        name: 'email',
-        message: questions[7],
+      type: "input",
+      name: "email",
+      message: questions[7],
     },
     {
-        type: 'input',
-        name: 'contents',
-        message: questions[8],
+      type: "input",
+      name: "contents",
+      message: questions[8],
     },
     {
-        type: 'list',
-        name: 'license',
-        message: questions[9],
-        choices: ['MIT', 'GNU', 'Apache', 'Mozilla', 'Boost', 'Unlicense'],
+      type: "list",
+      name: "license",
+      message: questions[9],
+      choices: ["MIT", "GNU", "Apache", "Mozilla", "Boost", "Unlicense"],
     },
-]).then((data) => {
-    const fileName = `${data.title.toLowerCase().split(' ').join('')}.md`;
-    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-    err ? console.error(err) : console.log('Success!')
-    );
-});
-
+  ])
+  .then((data) => {
+    const fileName = 'README.md';
+    const markdownContent = generateMarkdown(data);
+    writeToFile(fileName, markdownContent);
+  });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,7 +13,29 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}
 
+## Table of Contents
+${data.contents}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contribution
+${data.contribution}
+
+## Tests
+${data.test}
+
+## Questions
+If you have any questions, please feel free to contact me at [${data.email}](mailto:${data.email}). You can also find more of my work at [${data.github}](https://github.com/${data.github}).
+
+## License
+This project is licensed under the ${data.license} license.
 `;
 }
 
